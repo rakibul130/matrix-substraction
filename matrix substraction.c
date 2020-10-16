1399a85992f468d2ef5f9ac013b8b100cb2b0485
rakibul130 <72154626+rakibul130@users.noreply.github.com>
2020-10-16 20:06:33 +0600

    Add files via upload

---
+++ matrix substraction.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main(){
+       int i=3,j=3;
+       int a[i][j];
+       int b[i][j];
+       int c[i][j];
+       printf("enter rows of matrix a\n");
+       for(i=0;i<3;i++){
+           for(j=0;j<3;j++){
+               scanf("%d",&a[i][j]);
+           }
+       }
+       printf("enter rows of matrix b\n");
+       for(i=0;i<3;i++){
+          for(j=0;j<3;j++){
+               scanf("%d",&b[i][j]);
+
+          }
+      }
+      for(i=0;i<3;i++){
+         for(j=0;j<3;j++){
+            c[i][j]=a[i][j]-b[i][j];
+        }
+     }
+     printf("result of addition matrix \n");
+     for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+           printf("%d  ",c[i][j]);
+        }
+        printf("\n");
+     }
+ }
